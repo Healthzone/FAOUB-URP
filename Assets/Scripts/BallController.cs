4bece2fd4cc2da91aba999119c712d1ef4b3b7fa
Healthzone <emelyanov-89@list.ru>
2021-09-11 15:03:11 +0300

    Initial git

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Cinemachine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+    public float ForceMultiply;
+    public Rigidbody RB;
+    public Vector3 VecSpeed;
+    public LineRenderer Line;
+    public Camera cam;
+    public Transform cord;
+    public Transform BallTransform;
+    
+   
+
+
+   
+
+
+    private void OnMouseDrag()
+    {
+        Line.enabled = true;
+        Line.SetPosition(0, new Vector3(BallTransform.position.x, BallTransform.position.y, 0));
+        cord.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x , Input.mousePosition.y , 0));
+        Line.SetPosition(1, new Vector3(cord.position.x, cord.position.y, 0));
+        VecSpeed = new Vector3(cord.position.x - BallTransform.position.x, cord.position.y - BallTransform.position.y, 0);
+        
+    }
+
+    private void OnMouseUp()
+    {
+        Line.enabled = false;
+        RB.AddForce(-VecSpeed * ForceMultiply);
+        
+       
+    }
+
+}
