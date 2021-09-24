97c06a800b5a587351d84b638d36d9e6589f1f65
Healthzone <emelyanov-89@list.ru>
2021-09-25 02:15:59 +0300

    Переписал управление под виртуальный джойстик

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,36 +3,65 @@ using System.Collections;
 
 public class BallController : MonoBehaviour
 {
-    public float ForceMultiply;
-    public Rigidbody RB;
-    public Vector3 VecSpeed;
-    public LineRenderer Line;
-    public Camera cam;
-    public Transform cord;
-    public Transform BallTransform;
-    
-   
+
+    [SerializeField] private float forceMultiplier;
+    [SerializeField] private float lineRenderDistance = 3;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Vector3 vectorSpeed;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Transform lineTransform;
+    [SerializeField] private Transform ballTransform;
+
+    [SerializeField] private FixedJoystick fixedJoystick;
 
 
-   
 
 
-    private void OnMouseDrag()
+
+    private void Awake()
     {
-        Line.enabled = true;
-        Line.SetPosition(0, new Vector3(BallTransform.position.x, BallTransform.position.y, 0));
-        cord.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x , Input.mousePosition.y , 0));
-        Line.SetPosition(1, new Vector3(cord.position.x, cord.position.y, 0));
-        VecSpeed = new Vector3(cord.position.x - BallTransform.position.x, cord.position.y - BallTransform.position.y, 0);
-        
+        fixedJoystick = GameObject.FindGameObjectWithTag("Player").GetComponent<FixedJoystick>();
     }
 
-    private void OnMouseUp()
+    private void Update()
     {
-        Line.enabled = false;
-        RB.AddForce(-VecSpeed * ForceMultiply);
-        
-       
+        if (fixedJoystick.IsUsedJoystick)
+        {
+            SetPlayerMovement();
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+            rb.AddForce(-vectorSpeed * forceMultiplier);
+            vectorSpeed = Vector3.zero;
+            lineRenderer.SetPosition(0, Vector3.zero);
+            lineRenderer.SetPosition(1, Vector3.zero);
+        }
+    }
+
+
+    private void SetPlayerMovement()
+    {
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, ballTransform.position);
+
+
+
+        Vector3 lineSift = new Vector3(fixedJoystick.Direction.x * lineRenderDistance, fixedJoystick.Direction.y * lineRenderDistance, 0) + ballTransform.position;
+
+        lineRenderer.SetPosition(1, lineSift);
+
+
+
+        // lineTransform.position = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        //Vector3 direction = new Vector3(fixedJoystick.Horizontal, fixedJoystick.Vertical, ballTransform.position.z);
+        //Debug.Log(direction);
+        //lineTransform.localPosition = dialingSpeedFactor * direction;
+        //lineRenderer.SetPosition(1, new Vector3(lineTransform.localPosition.x, lineTransform.localPosition.y, lineTransform.localPosition.z));
+        vectorSpeed = new Vector3(lineSift.x - ballTransform.localPosition.x, lineSift.y - ballTransform.localPosition.y, 0);
+
+        Debug.Log(lineSift);
     }
 
 }
