49767fb45982047e912802e540bae6d8ea7c3270
healthzone <emelyanov.valek@gmail.com>
2023-01-17 19:28:41 +0300

    Добавил музыку Мелкие правки багов

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] _musics;
+    [SerializeField] private AudioSource _audioSource;
+    private int currentClipIndex = 0;
+
+    private void Start()
+    {
+        _audioSource.clip = _musics[currentClipIndex];
+        _audioSource.Play();
+    }
+
+    void Update()
+    {
+        if (!_audioSource.isPlaying)
+        {
+            _audioSource.clip = GetNextClip();
+            _audioSource.Play();
+        }
+    }
+
+    private AudioClip GetNextClip()
+    {
+        if (currentClipIndex >= _musics.Length - 1)
+        {
+            currentClipIndex = 0;
+        }
+        else
+        {
+            currentClipIndex += 1;
+        }
+        return _musics[currentClipIndex];
+    }
+}
--- Assets/Scripts/Player Scripts/DeathTrigger.cs
+++ Assets/Scripts/Player Scripts/DeathTrigger.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class DeathTrigger : MonoBehaviour
 {
     [SerializeField] private CheckpointSystem _checkpointSystem;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            _audioSource.Play();
             _checkpointSystem.UseCheckpoint();
         }
     }
